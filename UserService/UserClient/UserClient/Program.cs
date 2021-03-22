12607194491c37e677d328133d2a25d7398b8a34
Valent <80772449+Vlad-Valentin@users.noreply.github.com>
2021-03-22 13:38:51 +0200

    Replaced the generic while loop in the Main function with a CancellationTokenSource for keeping the UserClient window open.

--- UserService/UserClient/UserClient/Program.cs
+++ UserService/UserClient/UserClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Net.Client;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UserClient
@@ -11,45 +12,29 @@ namespace UserClient
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new UserManager.UserManagerClient(channel);
 
-            bool isActive = true;
+            var cancellationToken = new CancellationTokenSource(Timeout.Infinite);
 
-            while (isActive)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                Console.WriteLine("0. Exit\n1. Register User\n");
-                string option = Console.ReadLine();
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("CNP: ");
+                var cnp = Console.ReadLine();
 
-                switch (option)
+                var userToBeAdded = new User() { Name = name.Trim().Length > 0 ? name : "Invalid Name", CNP = cnp.Trim().Length == 13 ? cnp : "Invalid ID" };
+                var response = await client.AddUserAsync(new AddUserRequest { User = userToBeAdded });
+
+                if (response.Status == AddUserResponse.Types.Status.Success)
                 {
-                    case "1":
-                        Console.Write("\nName: ");
-                        var name = Console.ReadLine();
-                        Console.Write("CNP: ");
-                        var cnp = Console.ReadLine();
-
-                        var userToBeAdded = new User() { Name = name.Trim().Length > 0 ? name : "Invalid Name", CNP = cnp.Trim().Length == 13 ? cnp : "Invalid ID" };
-                        var response = await client.AddUserAsync(new AddUserRequest { User = userToBeAdded });
-
-                        if (response.Status == AddUserResponse.Types.Status.Success)
-                        {
-                            Console.WriteLine($"\nResponse Status: {response.Status}\n\nName: {response.Name}\nGender: {response.Gender}\nAge: {response.Age}\n");
-                        }
-                        else if (response.Name.Equals("Invalid Name"))
-                        {
-                            Console.WriteLine($"\nResponse Status: {response.Status}\nInvalid Name!\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"\nResponse Status: {response.Status}\nInvalid ID!\n");
-                        }
-                        break;
-
-                    case "0":
-                        isActive = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid Option!\n");
-                        break;
+                    Console.WriteLine($"\nResponse Status: {response.Status}\n\nName: {response.Name}\nGender: {response.Gender}\nAge: {response.Age}\n");
+                }
+                else if (response.Name.Equals("Invalid Name"))
+                {
+                    Console.WriteLine($"\nResponse Status: {response.Status}\nInvalid Name!\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nResponse Status: {response.Status}\nInvalid ID!\n");
                 }
             }
         }
